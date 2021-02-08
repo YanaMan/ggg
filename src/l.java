582f8ac5b8b0ac39bb5c38d00e1fc4261365b3fa
YanaMan <manaevaiana@gmail.com>
2021-02-08 18:57:39 +0300

    Initial commit

---
+++ src/l.java
@@ -0,0 +1,8 @@
+public class l {
+    public static void main(String[] args) {
+        System.out.println(2+9);
+        System.out.println(6*8);
+        System.out.println(95/5);
+        System.out.println(365/25);
+    }
+}
